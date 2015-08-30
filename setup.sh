055281b43655b6c2aabbb9a14046e404646ee519
Dariusz Dwornikowski <dariusz.dwornikowski@cs.put.poznan.pl>
2015-08-30 18:44:32 +0200

    Added vim installation function

--- setup.sh
+++ setup.sh
@@ -39,4 +39,16 @@ for file in $files; do
   ln -s "$DOTFILES/$file" "$HOME/.$file"
 done 
 
+setup_vim() {
+  if [ ! -d $HOME/.vim ]
+  then
+    git clone https://github.com/tdi/vimrc.git ~/.vim/
+    rm -f $HOME/.vimrc 
+    ln -s $HOME/.vim/vimrc $HOME/.vimrc
+    git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
+  fi
+}
+
+setup_vim 
 setup_ohmyzsh
